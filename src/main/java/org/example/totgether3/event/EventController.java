341702a9ab96a449e8d974cf6cae60c3f55aa015
Alexander Medvedev <its.alexander.medvedev@gmail.com>
2025-01-02 19:18:18 +0300

    req body

--- src/main/java/org/example/totgether3/event/EventController.java
+++ src/main/java/org/example/totgether3/event/EventController.java
@@ -3,10 +3,7 @@ package org.example.totgether3.event;
 import lombok.RequiredArgsConstructor;
 import org.example.totgether3.event.dto.CreateEventRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +14,7 @@ public class EventController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createEvent(CreateEventRequest createEventRequest) {
+    public void createEvent(@RequestBody CreateEventRequest createEventRequest) {
         eventService.createEvent(createEventRequest);
     }
 
